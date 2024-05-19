a4fa7dde8607fbbd9964db74dae7f43a4e889303
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-05-18 17:00:39 -0700

    🐛 react: fix pragma

--- packages/react/src/pragma/jsx-runtime.tsx
+++ packages/react/src/pragma/jsx-runtime.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { injectWorldIntoChildren } from './utils/inject-world-into-children';
-import { wrapRef } from './utils/wrap-ref';
 import { Entity } from '../entity/entity';
 import { SweetNode } from './types/types';
 import { World } from '../world/world';
@@ -35,48 +33,26 @@ export const jsx = (type: any, props: Record<string, any>, maybeKey?: React.Key)
 		key,
 		ref,
 		props: finalProps,
-		entityId: -1,
-		world: undefined!,
 	};
-
-	// Wrap the ref and merge it.
-	if (typeof node.type === 'string') node.props = { ref: wrapRef(ref, node), ...node.props };
 
 	// Handle `entity` intrinsic element.
 	if (type === 'entity') node.type = Entity;
 
 	// Handle `world` intrinsic element.
-	if (type === 'world') node.type = createWrappedWorld(node);
+	if (type === 'world') node.type = World;
 
-	// If we are rendering `World`, start passing the world object down.
-	if (type?.name === 'World') node.type = createWrappedWorld(node);
-
-	// If it is a functional component, we wrap it and inject the world into its results.
-	if (typeof type === 'function' && type?.name !== 'World') {
-		node.type = (props: Record<string, any>) => {
-			const result = type(props);
-
-			if (typeof result === 'object' && result !== null) {
-				// If we run into a processed context node, it has extensions prevented and we have to skip it.
-				if (!result.hasOwnProperty('_store')) result.world = node.world;
-				if (node.props.children) injectWorldIntoChildren(node, node.world);
-			}
-
-			return result;
+	// Wrap JSX instrisics in an entity.
+	if (typeof node.type === 'string') {
+		const wrappedNode = {
+			$$typeof: REACT_ELEMENT_TYPE,
+			type: Entity,
+			key,
+			ref: undefined,
+			props: { children: node },
 		};
+
+		return wrappedNode;
 	}
 
 	return node;
-};
-
-const createWrappedWorld = (node: SweetNode) => {
-	return (props: Record<string, any>) => {
-		const result = World(props);
-		node.world = result.props.value;
-
-		// Inject the world object into the children.
-		if (node.props.children) injectWorldIntoChildren(node, node.world);
-
-		return result;
-	};
 };
--- packages/react/src/pragma/types/types.ts
+++ packages/react/src/pragma/types/types.ts
@@ -1,11 +1,7 @@
-import { World as WorldCore } from '@sweet-ecs/core';
-
 export type SweetNode = {
 	$$typeof: Symbol;
 	type: any;
 	key: string | null;
 	ref: React.Ref<any>;
 	props: Record<string, any>;
-	entityId: number;
-	world: WorldCore;
 };
