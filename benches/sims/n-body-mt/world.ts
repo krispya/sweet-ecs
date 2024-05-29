843a9dfe4074755ea3d21750276f9047229dca22
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-05-29 08:51:29 -0700

    🐛 @sim/n-body-mt: increase world size

--- benches/sims/n-body-mt/world.ts
+++ benches/sims/n-body-mt/world.ts
@@ -1,6 +1,6 @@
 import { World } from '@sweet-ecs/core';
 import { CONSTANTS, Workers, Time } from '.';
 
-export const world = new World(CONSTANTS.NBODIES + 1);
+export const world = new World(CONSTANTS.NBODIES + 2);
 world.add(Time);
 world.add(Workers);
