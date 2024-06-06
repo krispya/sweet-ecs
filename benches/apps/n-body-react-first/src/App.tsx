00ac4e2fd051dea91fbea18e471feb2c9211d0c9
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-06-05 19:17:21 -0700

    🚧 @app/react-first: Shaping up

--- benches/apps/n-body-react-first/src/App.tsx
+++ benches/apps/n-body-react-first/src/App.tsx
@@ -1,3 +1,5 @@
+import { initStats } from '@app/bench-tools';
+import { Canvas } from '@react-three/fiber';
 import {
 	Acceleration,
 	CONSTANTS,
@@ -5,102 +7,139 @@ import {
 	Color,
 	Mass,
 	Position,
-	Time,
 	Velocity,
+	init,
 	randInRange,
+	schedule,
+	setInitial,
 	world,
 } from '@sim/n-body-soa';
-import { universe } from '@sweet-ecs/core';
-import { use, useEffect, useMemo, useRef, useState } from 'react';
-import './App.css';
-import Sweet from '@sweet-ecs/react';
+import { Component, Entity } from '@sweet-ecs/core';
+import Sweet, { useWorld } from '@sweet-ecs/react';
+import { useEffect, useMemo } from 'react';
+import * as THREE from 'three';
+import { ThreeInstances } from './components/ThreeInstances';
+import { syncThreeObjects } from './systems/syncThreeObjects';
+import { useRaf } from './use-raf';
+import { Spawner } from './spawner';
 
-export function App({ x, y, entityId }) {
-	const ref = useRef(null!);
-	const position = useCompontent(Position, ref);
+// Add view systems to the schedule
+schedule.add(syncThreeObjects, { after: 'update' });
+// Remove init systems
+// @ts-expect-error - library bug
+schedule.remove(init);
+// @ts-expect-error - library bug
+schedule.remove(setInitial);
+schedule.build();
 
-	useEffect(() => {
-		position.x = x;
-		position.y = y;
-	}, [x, y, position]);
+const BodySpawner = new Spawner(Body);
+console.log(BodySpawner);
+
+console.log(world);
+
+export function App() {
+	const frustumSize = 7000;
+	const aspect = window.innerWidth / window.innerHeight;
 
 	return (
-		<world src={world} size={CONSTANTS.NBODIES + 1} resources={[Time]}>
-			<Sweet.Entity
-				ref={ref}
-				components={[Position, Mass, Velocity, Acceleration, Color, Circle]}
+		<Sweet.World src={world}>
+			<Simulation />
+			<Canvas
+				orthographic
+				camera={{
+					left: (-frustumSize * aspect) / 2,
+					right: (frustumSize * aspect) / 2,
+					top: frustumSize / 2,
+					bottom: -frustumSize / 2,
+					near: 0.1,
+					far: 500,
+					position: [0, 0, 100],
+				}}
 			>
-				<div />
-			</Sweet.Entity>
-
-			<Sweet.Entity components={[Position]}>
-				<mesh />
-			</Sweet.Entity>
-
-			<div>
-				<Position.Component x={x} y={y} />
-			</div>
-
-			{Spawn(Body, count)}
-		</world>
+				<Bodies />
+				<BodySpawner.Emitter initial={CONSTANTS.NBODIES - 1} />
+			</Canvas>
+		</Sweet.World>
 	);
 }
 
-function Body({ children }: { children?: React.ReactNode }) {
-	const position = useMemo(() => ({ x: randInRange(-4000, 4000), y: randInRange(-100, 100) }), []);
-	const mass = useMemo(() => CONSTANTS.BASE_MASS + randInRange(0, CONSTANTS.VAR_MASS), []);
+function Bodies() {
+	const world = useWorld();
+	const geo = useMemo(() => new THREE.CircleGeometry(CONSTANTS.MAX_RADIUS / 1.5, 12), []);
+	const mat = useMemo(() => new THREE.MeshBasicMaterial(), []);
 
-	const velocity = useMemo(() => {
-		if (position.x === 0 && position.y === 0) return { x: 0, y: 0 };
+	return (
+		<instancedMesh
+			ref={(node) => {
+				if (!node) return;
+				const eid = Entity.in(world);
+				Entity.add(new ThreeInstances(node), eid);
 
-		// Calculate velocity for a stable circular orbit
-		const radius = Math.sqrt(position.x ** 2 + position.y ** 2);
-		const normX = position.x / radius;
-		const normY = position.y / radius;
+				return () => {
+					Entity.destroy(eid);
+				};
+			}}
+			args={[geo, mat, CONSTANTS.NBODIES]}
+		/>
+	);
+}
 
-		// Perpendicular vector for circular orbit
-		const vecRotX = -normY;
-		const vecRotY = normX;
+function Body({ components = [] }: { components: (typeof Component | Component)[] }) {
+	const position = useMemo(() => {
+		return new Position(randInRange(-4000, 4000), randInRange(-100, 100));
+	}, []);
 
-		const v = Math.sqrt(CONSTANTS.INITIAL_C / radius / mass / CONSTANTS.SPEED);
-		return { x: vecRotX * v, y: vecRotY * v };
-	}, [position, mass]);
+	const mass = useMemo(() => {
+		return new Mass(CONSTANTS.BASE_MASS + randInRange(0, CONSTANTS.VAR_MASS));
+	}, []);
 
-	const radius = useMemo(() => {
-		return CONSTANTS.MAX_RADIUS * (mass / (CONSTANTS.BASE_MASS + CONSTANTS.VAR_MASS)) + 1;
+	const circle = useMemo(() => {
+		return new Circle(
+			CONSTANTS.MAX_RADIUS * (mass.value / (CONSTANTS.BASE_MASS + CONSTANTS.VAR_MASS)) + 1
+		);
 	}, [mass]);
 
+	const velocity = useMemo(() => {
+		const { x, y } = calcStableVelocity(position.x, position.y, mass.value);
+		return new Velocity(x, y);
+	}, [mass.value, position.x, position.y]);
+
 	return (
-		<entity>
-			<Mass.Component value={mass} />
-			<Position.Component x={position.x} y={position.y} />
-			<Velocity.Component x={velocity.x} y={velocity.y} />
-			<Circle.Component radius={radius} />
-			<Color.Component />
-			<Acceleration.Component />
-			{children}
-		</entity>
+		<Sweet.Entity
+			components={[position, mass, velocity, circle, Acceleration, Color, ...components]}
+		/>
 	);
 }
 
-function Spawn(Element: React.FunctionComponent, count: number) {
-	const elements = useRef<React.ReactElement[]>(null!);
+// Simulation runs a schedule.
+function Simulation() {
+	const statsAPI = useMemo(() => initStats({ Bodies: () => CONSTANTS.NBODIES }), []);
+	const world = useWorld();
 
-	elements.current ??= Array.from({ length: count }, (_, index) => {
-		return <Element key={index} />;
+	useEffect(() => {
+		statsAPI.create();
+		return () => statsAPI.destroy();
 	});
 
-	if (elements.current.length < count) {
-		elements.current = elements.current.concat(
-			Array.from({ length: count - elements.current.length }, (_, index) => {
-				return <Element key={index + elements.current.length} />;
-			})
-		);
-	}
+	useRaf(() => {
+		statsAPI.measure(() => {
+			schedule.run({ world });
+		});
+		statsAPI.updateStats();
+	}, [world, statsAPI]);
 
-	if (elements.current.length > count) {
-		elements.current = elements.current.slice(0, count);
-	}
+	return null;
+}
 
-	return elements.current;
+function calcStableVelocity(x: number, y: number, mass: number) {
+	const radius = Math.sqrt(x ** 2 + y ** 2);
+	const normX = x / radius;
+	const normY = y / radius;
+
+	// Perpendicular vector for circular orbit
+	const vecRotX = -normY;
+	const vecRotY = normX;
+
+	const v = Math.sqrt(CONSTANTS.INITIAL_C / radius / mass / CONSTANTS.SPEED);
+	return { x: vecRotX * v, y: vecRotY * v };
 }
