65e8047ebf6ca6243cd0a503b81f99bbc6c810bb
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-06-12 17:14:08 -0700

    🔥 react: Remove default export

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,21 +1,3 @@
-import { World } from './world/world';
-import { useWorld } from './world/use-world';
-import { WorldContext } from './world/world-context';
-import { Entity } from './entity/entity';
-import { Component } from './component/component';
-import { useEntity } from './entity/use-entity';
-
-const Sweet = {
-	World,
-	WorldContext,
-	useWorld,
-	Entity,
-	Component,
-	useEntity,
-};
-
-export default Sweet;
-
 export { World } from './world/world';
 export { useWorld } from './world/use-world';
 export { WorldContext } from './world/world-context';
