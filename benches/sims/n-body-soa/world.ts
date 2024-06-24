21b1f4ff084af485d5b5d96ae6fc907647783891
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-06-23 21:09:09 -0700

    🐛 @sim/n-body-soa: Fix schedule

--- benches/sims/n-body-soa/world.ts
+++ benches/sims/n-body-soa/world.ts
@@ -1,6 +1,5 @@
 import { World } from '@sweet-ecs/core';
-import { CONSTANTS } from '.';
 import { Time } from './components/Time';
 
-export const world = new World(CONSTANTS.NBODIES * 2);
+export const world = new World();
 world.add(Time);
