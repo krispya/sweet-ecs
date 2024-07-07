a06e48400b116dddafaa9356fb16a7aa5fb39c7e
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-07-07 14:09:12 -0700

    ♻️ core: SoA components now have enumerable properties

--- packages/core/src/component/utils/create-soa-component.ts
+++ packages/core/src/component/utils/create-soa-component.ts
@@ -5,8 +5,7 @@ type ComponentInstance<T = {}, TSchema extends Schema = {}> = Component &
 	PropsFromSchema<TSchema> &
 	T;
 
-// Declaraing the accessors on the class definition gives a large performance boost
-// compared to using `defineProperties` on the prototype. So we eval it.
+// Evaling the class definition with static keys optimizes performance.
 
 export function createSoAComponent<T = {}, TSchema extends Schema = {}>(
 	schema: TSchema,
@@ -36,7 +35,23 @@ export function createSoAComponent<T = {}, TSchema extends Schema = {}>(
 					this[$initialState] = () => initialState;
 				}
 			}
-		}
+		`;
+
+	// Add getters/setters to the instance so they are enumberable.
+	for (const key in schema) {
+		classDefinition += `Object.defineProperty(this, '${key}', {
+			get() {
+				return this.constructor.store['${key}'][this[$entityId]];
+			},
+			set(value) {
+				this.constructor.store['${key}'][this[$entityId]] = value;
+			},
+			enumerable: true,
+		});
+		`;
+	}
+
+	classDefinition += `}
 
 		set(state) {
 			if (typeof state === 'function') state = state();
@@ -45,22 +60,7 @@ export function createSoAComponent<T = {}, TSchema extends Schema = {}>(
 			}
 			return this;
 		}
-  	`;
-
-	// Dynamically add getters and setters for each schema property.
-	for (const key of Object.keys(schema)) {
-		classDefinition += `
-		get ${key}() {
-			return this.constructor.store['${key}'][this[$entityId]];
-		}
-
-		set ${key}(value) {
-			this.constructor.store['${key}'][this[$entityId]] = value;
-		}
-	`;
-	}
-
-	classDefinition += `};
+	};
   	return SoAComponent;
   	`;
 
